a54e2760498b5c5a12de1b1ac20636b32d7fb478
Suyash Pandey <86300308+Suyash699@users.noreply.github.com>
2023-04-20 21:25:19 +0530

    update naive solution

--- Problem Solving Patterns/Multiple Pointers/naiveSoln.js
+++ Problem Solving Patterns/Multiple Pointers/naiveSoln.js
@@ -1,1 +1,15 @@
 //new file
+//Wtite a function called sumZero which accepts a sorted array of integers.
+//The function should find the first pair where sum is zero. Return the array that includes both values that sum to zero or undefined if pair does not exist
+
+function sumZero(){
+  for(let i=0; i<arr.length; i++){
+    for(let j=i+1; j<arr.length; j++){
+      if(arr[i] + arr[j] === 0){
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
+
+sumZero([-4, -3, -2, -1, 0, 1, 2, 5])
