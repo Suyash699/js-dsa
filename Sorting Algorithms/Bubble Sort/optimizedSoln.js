00e3aaa875b5cb6aa2e2334447b147d3e92a710f
Suyash Pandey <86300308+Suyash699@users.noreply.github.com>
2023-07-07 21:18:37 +0530

    Create optimizedSoln.js

---
+++ Sorting Algorithms/Bubble Sort/optimizedSoln.js
@@ -0,0 +1,1 @@
+//The problem with naive is that it keeps comparing even if the array is sorted as long as the end is not reached in the loop so we add a noSwaps variable after the i loop & set its value as true inside the j loop in if condition i.e., if the swap is made, set noSwaps as false and at the end of j loop if noSwaps is true, break.
